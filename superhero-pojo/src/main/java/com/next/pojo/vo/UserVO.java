528035b1714ac8d4bf26301f8a132424d8d2f974
zhouxiaoping <zhouxiaoping@hztianque.com>
2019-11-07 16:56:08 +0800

    删除密码字段

--- superhero-pojo/src/main/java/com/next/pojo/vo/UserVO.java
+++ superhero-pojo/src/main/java/com/next/pojo/vo/UserVO.java
@@ -9,31 +9,17 @@ import java.util.Date;
 @Getter
 public class UserVO {
     private String id;
-
     private String username;
-
-    private String password;
-
     private String nickname;
-
     private String mpWxOpenId;
-
     private String appQqOpenId;
-
     private String appWxOpenId;
-
     private String appWeiboUid;
-
     private Integer sex;
-
     private String birthday;
-
     private String faceImage;
-
     private Integer isCertified;
-
     private Date registTime;
-
     /**
      * 用户唯一令牌
      */
